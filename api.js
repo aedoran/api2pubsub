7c9e92598c46278e960e8de85c5768bee12adbda
Tony <aedoran@gmail.com>
2012-05-31 23:38:40 -0400

    initial commit

---
+++ api.js
@@ -0,0 +1,19 @@
+module.exports = {
+  findAppointment : function(args, cb) {
+
+    //simulate the api call
+    setInterval(
+        function() {
+          console.log("API CALL");
+          cb(null,{data:"appointment"});
+        },
+        10000);
+    
+  },
+  findAlerts : function(args,cb) {
+    setInterval(function() {
+      console.log("API CALL ALERT");
+      cb(null,{data:"alert"});
+    },1000);
+  }
+}
---
+++ main.js
@@ -0,0 +1,59 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , api = require('./api.js')
+
+app.listen(80);
+
+io.set('log level',1);
+
+//map of needs, this will poplulate the needs being requested by the client
+var need_cache = {};
+
+
+//a map of need to api calls
+var api_config = {
+  'appointment' : api.findAppointment,
+  'alert' : api.findAlerts,
+};
+
+
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.on("needs",function(data) {
+    console.log("Recieved needs:",data);
+    
+    data.needs.forEach(function(need) {
+      console.log(need);
+
+      //check if need is being satisfied
+      if (!need_cache[need]) {
+      
+        //create calls for need, and emit response on the data from the api
+        console.log("Making calls for "+need);
+        need_cache[need] = 1;
+        api_config[need]({},function (err,data) {
+          console.log("Fullfilling need:"+need);
+          socket.emit(need,data);
+          socket.broadcast.emit(need,data);
+        });
+      }
+    });
+  });
+
+});
